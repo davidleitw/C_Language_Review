424f8bf4705faf2515ed77c2427d1aa378954d08
davidleitw <davidleitw@yahoo.com.tw>
2019-04-06 02:10:48 +0800

    example_1 for pointer

--- c_pointer_review/example_1.cpp
+++ c_pointer_review/example_1.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 using namespace std;
+/*
+    感謝好友@阿盧分享此題目，記錄此題操作方法在github
+*/
 int main()
 {
     int a[5] = {1, 2, 3, 4, 5};
     int *p = (int *)(&a + 1);
    
+    // 最重要的就是這兩個的差距
+    // a + 1 and &a + 1 
     cout << a+1 << " " << &a+1 << endl;
 
     for(int i = 0; i < 5; ++i)
@@ -13,7 +18,7 @@ int main()
 
     cout << "p = " << p << endl;
     cout << "*p = " << *p << endl;
-    cout << "(*p-1) = " << (*p - 1) << endl;
-    cout << "*(p-1) = " << *(p - 1) << endl;
+    cout << "(*p-1) = " << (*p - 1) << endl; // = a[5] - 1 (undefined)
+    cout << "*(p-1) = " << *(p - 1) << endl; // = a[5-1] = a[4] = 5 (defined)
     return 0;
 }
